46f3915a68ffbb58cc145586192c54f0ad766d86
Ravi Kumar <you@example.com>
2020-11-30 21:41:43 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,47 @@
 import { Component } from '@angular/core';
-
+import {jsPDF} from 'jspdf';
+import 'jspdf-autotable';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-pdf';
+  title = 'jspdf-autotable-demo';
+
+  head = [['ID', 'Country', 'Rank', 'Capital']]
+
+  data = [
+    [1, 'Finland', 7.632, 'Helsinki'],
+    [2, 'Norway', 7.594, 'Oslo'],
+    [3, 'Denmark', 7.555, 'Copenhagen'],
+    [4, 'Iceland', 7.495, 'Reykjavík'],
+    [5, 'Switzerland', 7.487, 'Bern'],
+    [9, 'Sweden', 7.314, 'Stockholm'],
+    [73, 'Belarus', 5.483, 'Minsk'],
+  ]
+
+  createPdf() {
+    var doc = new jsPDF();
+    doc.setFontSize(18);
+    doc.text('My PDF Table', 11, 8);
+    doc.setFontSize(11);
+    doc.setTextColor(100);
+    (doc as any).autoTable({
+      head: this.head,
+      body: this.data,
+      theme: 'plain',
+      didDrawCell: data => {
+        console.log(data.column.index)
+      }
+    })
+
+    // Open PDF document in new tab
+    doc.output('dataurlnewwindow')
+
+    // Download PDF document  
+    doc.save('table.pdf');
+  }
+
+
 }
